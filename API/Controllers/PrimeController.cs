c76482119f7723f172b8ffdffd3fb86d9c5b08f7
Jader Dalmas <jaderpx@hotmail.com>
2021-04-22 18:18:38 -0300

    prime controller

---
+++ API/Controllers/PrimeController.cs
@@ -0,0 +1,24 @@
+﻿using GrainInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using Orleans;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+  [ApiController, Route("[controller]")]
+  public class PrimeController : ControllerBase
+  {
+    private readonly IGrainFactory _client;
+    public PrimeController(IGrainFactory client)
+    {
+      _client = client;
+    }
+
+    [HttpGet("{number}")]
+    public async Task<bool> Get(int number = 101)
+    {
+      var grain = _client.GetGrain<IPrime>(0);
+      return await grain.IsPrime(number);
+    }
+  }
+}
