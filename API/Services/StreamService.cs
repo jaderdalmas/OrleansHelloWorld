7218668ef75f300c211c4a34bd274a2c47336a00
Jader Dalmas <jaderpx@hotmail.com>
2021-04-26 10:42:45 -0300

    change to isprime initialization

--- API/Services/StreamService.cs
+++ API/Services/StreamService.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Orleans;
 using Orleans.Streams;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,21 +23,35 @@ namespace API.Services
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-      var grain = _client.GetGrain<IHello>(0);
+      var grain = _client.GetGrain<IPrime>(0);
       var key = grain.GetGrainIdentity().PrimaryKey;
 
-      var response = await grain.SayHello("Testing");
-      Console.WriteLine($"{response}");
+      var response = await grain.IsPrime(99);
+      Console.WriteLine($"IsPrime: {response}");
 
       var stream = _client.GetStreamProvider(AppConst.SMSProvider)
-        .GetStream<string>(key, AppConst.PSHello);
+        .GetStream<int>(key, AppConst.PSPrime);
       await stream.SubscribeAsync(OnNextAsync);
 
-      for (int i = 1; i < 10; i++)
-        await stream.OnNextAsync($"Good morning, {i}!");
+      for (int mil = 0; mil < 1; mil++)
+      {
+        var tasks = new List<Task>();
+
+        for (int dez = mil == 0 ? 100 : 0; dez < 1000; dez += 10)
+        {
+          var item = mil * 1000 + dez;
+
+          tasks.Add(stream.OnNextAsync(item + 1));
+          tasks.Add(stream.OnNextAsync(item + 3));
+          tasks.Add(stream.OnNextAsync(item + 7));
+          tasks.Add(stream.OnNextAsync(item + 9));
+        }
+
+        Task.WaitAll(tasks.ToArray());
+      }
     }
 
-    private Task OnNextAsync(string item, StreamSequenceToken? token = null)
+    private Task OnNextAsync(int item, StreamSequenceToken token = null)
     {
       _logger.LogInformation($"OnNextAsync: item: {item}, token = {token}");
       return Task.CompletedTask;
