f32171ef2921129eadc16ad0d1cb163c138fa512
Jader Dalmas <jaderpx@hotmail.com>
2021-04-27 12:12:31 -0300

    change into a more generic extension

---
+++ Grains/Extension/PrimeExtension.cs
@@ -0,0 +1,34 @@
+﻿namespace Grains
+{
+  public static class IntExtension
+  {
+    public static bool IsDivisible(this int number, int divisor)
+    {
+      return (decimal)number % divisor == 0;
+    }
+  }
+
+  public static class LongExtension
+  {
+    public static bool IsDivisible(this long number, long divisor)
+    {
+      return (decimal)number % divisor == 0;
+    }
+  }
+
+  public static class UIntExtension
+  {
+    public static bool IsDivisible(this uint number, uint divisor)
+    {
+      return (decimal)number % divisor == 0;
+    }
+  }
+
+  public static class ULongExtension
+  {
+    public static bool IsDivisible(this ulong number, ulong divisor)
+    {
+      return (decimal)number % divisor == 0;
+    }
+  }
+}
