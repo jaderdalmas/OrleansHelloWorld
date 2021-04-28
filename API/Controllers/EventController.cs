a79838130c966625c85f9a5e3abc0cd55024cb70
Jader Dalmas <jaderpx@hotmail.com>
2021-04-28 11:39:35 -0300

    try add event store

---
+++ API/Controllers/EventController.cs
@@ -0,0 +1,41 @@
+﻿using EventStore.ClientAPI;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+  [ApiController, Route("[controller]")]
+  public class EventController : ControllerBase
+  {
+    public EventController() { }
+
+    [HttpGet]
+    public async Task Get()
+    {
+      var conn = EventStoreConnection.Create(new Uri("tcp://localhost:1113"));
+      await conn.ConnectAsync();
+
+      const string streamName = "newstream";
+      const string eventType = "event-type";
+      const string data = "{ \"a\":\"2\"}";
+      const string metadata = "{}";
+
+      var eventPayload = new EventData(
+          eventId: Guid.NewGuid(),
+          type: eventType,
+          isJson: true,
+          data: Encoding.UTF8.GetBytes(data),
+          metadata: Encoding.UTF8.GetBytes(metadata)
+      );
+      _ = await conn.AppendToStreamAsync(streamName, ExpectedVersion.Any, eventPayload);
+
+      var readEvents = await conn.ReadStreamEventsForwardAsync(streamName, 0, 10, true);
+      foreach (var evt in readEvents.Events)
+      {
+        Console.WriteLine(Encoding.UTF8.GetString(evt.Event.Data));
+      }
+    }
+  }
+}
