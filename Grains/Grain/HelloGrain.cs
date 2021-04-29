889dcb89da4acea9f5f303cd7189bfbe4e6c5d05
Jader Dalmas <jaderpx@hotmail.com>
2021-04-29 15:20:04 -0300

    add event store on a grain

--- Grains/Grain/HelloGrain.cs
+++ Grains/Grain/HelloGrain.cs
@@ -1,9 +1,12 @@
-﻿using Interfaces;
+﻿using EventStore.Client;
+using Interfaces;
 using Interfaces.Model;
 using Microsoft.Extensions.Logging;
 using Orleans;
 using Orleans.Streams.Core;
 using System;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Grains
@@ -12,23 +15,33 @@ namespace Grains
   public class HelloGrain : Grain, IHello
   {
     private readonly ILogger _logger;
-    private readonly Observer<string> observer;
-    private int _counter;
 
-    public HelloGrain(ILogger<IHello> logger)
+    public HelloGrain(ILogger<IHello> logger, EventStoreClient client)
     {
       _logger = logger;
+
+      _client = client;
       observer = new Observer<string>(logger, (string greeting) => SayHello(greeting));
 
       _counter = 0;
     }
 
+    private readonly EventStoreClient _client;
+    private readonly Observer<string> observer;
     public async Task OnSubscribed(IStreamSubscriptionHandleFactory handleFactory)
     {
+      _client.SubscribeToStreamAsync(InterfaceConst.PSHello, SubscribeReturn).Wait();
+
       var handle = handleFactory.Create<string>();
       await handle.ResumeAsync(observer);
     }
+    private Task SubscribeReturn(EventStore.Client.StreamSubscription ss, ResolvedEvent vnt, CancellationToken ct)
+    {
+      var result = Encoding.UTF8.GetString(vnt.Event.Data.Span);
+      return SayHello(result);
+    }
 
+    private int _counter;
     public Task<string> SayHello(string greeting)
     {
       var counter = $"{++_counter} times!";
