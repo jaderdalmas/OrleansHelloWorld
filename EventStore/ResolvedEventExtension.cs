22c67ddaa07828d42ca93a39fe8236bf481529ae
Jader Dalmas <jaderpx@hotmail.com>
2021-05-05 16:37:40 -0300

    resolved event json and type conversion

---
+++ EventStore/ResolvedEventExtension.cs
@@ -0,0 +1,19 @@
+﻿using EventStore.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace EventStore
+{
+  public static class ResolvedEventExtension
+  {
+    public static string ToJson(this ResolvedEvent vnt)
+    {
+      return Encoding.UTF8.GetString(vnt.Event.Data.Span);
+    }
+
+    public static T To<T>(this ResolvedEvent vnt)
+    {
+      return JsonSerializer.Deserialize<T>(vnt.ToJson());
+    }
+  }
+}
