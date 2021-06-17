3be9731dc1783dc41849d38989c85449d7c0e01a
Jader Dalmas <jaderpx@hotmail.com>
2021-06-17 17:38:13 -0300

    aggregate service

---
+++ EventSourcing/Extension/ServiceExtension.cs
@@ -0,0 +1,15 @@
+﻿using EventSourcing.Aggregate;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventSourcing
+{
+  public static class ServiceExtension
+  {
+    public static IServiceCollection AddAggregateServices(this IServiceCollection service)
+    {
+      service.AddSingleton<PrimeAggregate>();
+
+      return service;
+    }
+  }
+}
