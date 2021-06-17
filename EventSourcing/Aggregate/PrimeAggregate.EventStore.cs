c65bbefe9dd3817391d83a4c0a5ce30ce3a1f3aa
Jader Dalmas <jaderpx@hotmail.com>
2021-06-17 17:37:09 -0300

    eventstore aggregate

---
+++ EventSourcing/Aggregate/PrimeAggregate.EventStore.cs
@@ -0,0 +1,50 @@
+﻿using EventSourcing.Event;
+using EventStore;
+using EventStore.Client;
+using Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSourcing.Aggregate
+{
+  public partial class PrimeAggregate : IAggregate
+  {
+    public IList<IsPrimeEvent> Events { get; private set; } = new List<IsPrimeEvent>();
+    public IEnumerable<int> PrimeEvents => Events.Where(x => x.Prime.Value).Select(x => x.Number);
+
+    private EventStoreClient _eventStore;
+
+    public void Initialize_ES(EventStoreClient eventStore)
+    {
+      _eventStore = eventStore;
+
+      var result = _eventStore.ReadStreamAsync(Direction.Forwards, InterfaceConst.ESPrime, StreamPosition.Start);
+      if (result.ReadState.Result == ReadState.StreamNotFound)
+        return;
+
+      foreach (var vnt in result.ToEnumerable())
+        Events.Add(vnt.To<IsPrimeEvent>());
+
+      foreach (var @event in Events)
+      {
+        if (!@event.Prime.HasValue)
+          IsPrime(@event.Number);
+        else if (@event.Prime.Value)
+          Primes.Add(@event.Number);
+      }
+    }
+
+    public async Task Emit(IEvent @event)
+    {
+      var vnt = @event as IsPrimeEvent;
+      Events.Add(vnt);
+
+      await _eventStore.AppendToStreamAsync(
+        InterfaceConst.ESPrime,
+        StreamState.Any,
+        new[] { vnt.GetEvent() }
+      );
+    }
+  }
+}
